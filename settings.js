30cf7b19d7dbddedf5cb86c9d3d4961529527e60
zaki-spec <151471536+zaki-spec@users.noreply.github.com>
2025-04-03 16:17:47 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
-  author: 'BLACKLIGHT',
-  botName: "BLACKLIGHT BOT",
-  botOwner: 'BLACKLIGHT, //
-  ownerNumber: '917023951514', //2349167400488
+  author: 'bot',
+  botName: "BLACKLIGH",
+  botOwner: 'professor, //
+  ownerNumber: '2349167400488', //
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
